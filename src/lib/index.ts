23df14098178250f4d0a5d4af49b3b83ba9d276e
rossrobino <rbrobino@gmail.com>
2023-05-25 14:12:49 -0700

    export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import Chord from "./components/Chord.svelte";
 import Editor from "./components/Editor.svelte";
+import FullscreenButton from "./components/FullscreenButton.svelte";
 import ShareButton from "./components/ShareButton.svelte";
 import YouTube from "./components/YouTube.svelte";
 
-export { Chord, Editor, ShareButton, YouTube };
+export { Chord, Editor, FullscreenButton, ShareButton, YouTube };
