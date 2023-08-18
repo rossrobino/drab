1cbad222be951993ceb5ca703601cf8565ae18e5
Ross Robino <ross@robino.dev>
2023-08-18 13:48:21 -0400

    disabled full screen button

--- src/routes/docs/FullscreenButton/+page.svelte
+++ src/routes/docs/FullscreenButton/+page.svelte
@@ -4,9 +4,9 @@
 	let target: HTMLDivElement;
 </script>
 
-<FullscreenButton class="btn mb-8" />
+<FullscreenButton class="btn" />
 
-<div bind:this={target} class="rounded bg-neutral-800 p-4 text-neutral-50">
+<div bind:this={target} class="mt-8 rounded bg-neutral-800 p-4 text-neutral-50">
 	<div class="mb-2">Target element</div>
 	<FullscreenButton {target} class="btn btn-s bg-neutral-50">
 		Enable Element Fullscreen
