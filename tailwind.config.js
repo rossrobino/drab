191146410aa89945824291eebc7d48c09bef62c9
rossrobino <rbrobino@gmail.com>
2023-09-08 08:40:38 -0400

    update uico

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,57 +4,5 @@ import { uico } from "uico";
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				antique: [
-					"Superclarendon",
-					"Bookman Old Style",
-					"URW Bookman",
-					"URW Bookman L",
-					"Georgia Pro",
-					"Georgia",
-					"serif",
-				],
-			},
-			borderColor: {
-				DEFAULT: "hsl(var(--border))",
-			},
-			colors: {
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
-				card: {
-					DEFAULT: "hsl(var(--card))",
-					foreground: "hsl(var(--card-foreground))",
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))",
-				},
-				muted: {
-					DEFAULT: "hsl(var(--muted))",
-					foreground: "hsl(var(--muted-foreground))",
-				},
-				primary: {
-					DEFAULT: "hsl(var(--primary))",
-					foreground: "hsl(var(--primary-foreground))",
-				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary))",
-					foreground: "hsl(var(--secondary-foreground))",
-				},
-				destructive: {
-					DEFAULT: "hsl(var(--destructive))",
-					foreground: "hsl(var(--destructive-foreground))",
-				},
-			},
-			borderRadius: {
-				lg: "calc(var(--radius) + 4px)",
-				md: "calc(var(--radius) + 2px)",
-				DEFAULT: "var(--radius)",
-				sm: "calc(var(--radius) - 2px)",
-			},
-		},
-	},
 	plugins: [typography, uico],
 };
