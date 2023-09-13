19b7b06bfe20c20e57a50d063c8cd3c3838d36d5
rossrobino <rbrobino@gmail.com>
2023-09-13 16:14:39 -0400

    config clean up

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-import typography from "@tailwindcss/typography";
-import { uico } from "uico";
-
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
-	plugins: [typography, uico],
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import typography from "@tailwindcss/typography";
+import { uico } from "uico";
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		postcss: {
+			plugins: [
+				tailwindcss({
+					content: ["./src/**/*.{html,js,svelte,ts}"],
+					plugins: [typography, uico],
+				}),
+				autoprefixer(),
+			],
+		},
+	},
 });
