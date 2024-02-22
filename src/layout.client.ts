ce9714227203b7ed72fbe296399438b5d5ea0ce9
rossrobino <rbrobino@gmail.com>
2024-02-22 15:26:25 -0500

    docs: add analytics

--- src/layout.client.ts
+++ src/layout.client.ts
@@ -1,2 +1,5 @@
 import "./package/dialog/define";
 import "./package/prefetch/define";
+
+import { inject } from "@vercel/analytics";
+inject({ mode: import.meta.env.DEV ? "development" : "production" });
