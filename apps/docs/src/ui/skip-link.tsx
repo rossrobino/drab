a1ed661834a3a1b2d4522cb0a5a3fe01e710aeca
rossrobino <rbrobino@gmail.com>
2025-02-11 13:29:56 -0500

    skip link left

--- apps/docs/src/ui/skip-link.tsx
+++ apps/docs/src/ui/skip-link.tsx
@@ -1,7 +1,7 @@
 export const SkipLink = () => {
 	return (
 		<a
-			class="button absolute -top-12 left-56 z-20 focus-visible:translate-y-16 motion-safe:transition-transform"
+			class="button absolute -top-12 left-4 z-20 focus-visible:translate-y-16 motion-safe:transition-transform lg:left-56"
 			href="#content"
 		>
 			Skip to Content
