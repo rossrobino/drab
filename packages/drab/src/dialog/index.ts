d99f9961062f2990046c522afeb27d0f8c86a822
Carlos Verdes <cverdes@gmail.com>
2025-04-25 15:18:18 +0400

    feat: allow close on outside click when dialog cover full viewport (#36)
    * feat: allow close on outside click when dialog cover full viewport
    
    * minor changes + add changeset

--- packages/drab/src/dialog/index.ts
+++ packages/drab/src/dialog/index.ts
@@ -8,12 +8,16 @@ export type DialogAttributes = BaseAttributes & {
 /**
  * Provides triggers for the `HTMLDialogElement`.
  *
+ * ### Attributes
+ *
  * `click-outside-close`
  *
  * By default, the `HTMLDialogElement` doesn't close if the user clicks outside of it.
  * Add a `click-outside-close` attribute to close when the user clicks outside.
  *
- * ### Attributes
+ * If the dialog covers the full viewport and this attribute is present, the first child
+ * of the dialog will be assumed to be the content of the dialog and the dialog will close
+ * if there is a click outside of the first child element.
  *
  * `remove-body-scroll`
  *
@@ -80,7 +84,17 @@ export class Dialog extends Base {
 		if (this.hasAttribute("click-outside-close")) {
 			// https://blog.webdevsimplified.com/2023-04/html-dialog/#close-on-outside-click
 			this.dialog.addEventListener("click", (e) => {
-				const rect = this.dialog.getBoundingClientRect();
+				let rect = this.dialog.getBoundingClientRect();
+
+				// If dialog covers full viewport (with a small tolerance), use first child element for hit testing
+				// Example: https://picocss.com/docs/modal
+				if (
+					rect.width - window.innerWidth <= 5 && // 5px tolerance for rounding issues
+					rect.height - window.innerHeight <= 5 &&
+					this.dialog.firstElementChild
+				) {
+					rect = this.dialog.firstElementChild.getBoundingClientRect();
+				}
 
 				if (
 					e.clientX < rect.left ||
