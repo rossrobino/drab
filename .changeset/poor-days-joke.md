f83a6ca974bc4a78d92c82e797b24505bc185c84
rossrobino <rbrobino@gmail.com>
2025-05-01 10:21:00 -0400

    share updates

--- .changeset/poor-days-joke.md
+++ .changeset/poor-days-joke.md
@@ -2,11 +2,24 @@
 "drab": major
 ---
 
-Copy
+Share/Copy
 
-The `Copy` element has been merged into `Share`. Replace `drab-copy` with `drab-share` and it will function the same with an enhancement---if the value starts with `http`, it will try to share the value before falling back to copy. `Share` works the same as before.
+`Share` now has three attributes:
+
+- `url`: URL to share.
+- `text`: Text to copy, or the `ShareData` text if `url` is set (only supported on some targets).
+- `share-title`: `ShareData` title (only supported on some targets).
+
+Update the `value` attribute to `url`.
+
+```diff
+- <drab-share value="https://...">...</drab-share>
++ <drab-share url="https://...">...</drab-share>
+```
+
+The `Copy` element has been merged into `Share`. Replace `drab-copy` with `drab-share` and it will function the same with an enhancement---if a `url` is provided, it will try to share the url before falling back to copy. If `text` it provided without a `url` it will function exactly the same.
 
 ```diff
 - <drab-copy value="some text">...</drab-copy>
-+ <drab-share value="some text">...</drab-share>
++ <drab-share text="some text">...</drab-share>
 ```
--- packages/drab/src/share/index.ts
+++ packages/drab/src/share/index.ts
@@ -7,56 +7,93 @@ import {
 	type ContentAttributes,
 } from "../base/index.js";
 
-export interface ShareAttributes extends TriggerAttributes, ContentAttributes {
-	value: string;
-}
+export type ShareAttributes = TriggerAttributes &
+	ContentAttributes &
+	(
+		| {
+				/** Share URL */
+				url: string;
+
+				/** `ShareData` text (only supported on some targets) */
+				text?: string;
+
+				/** Share title */
+				"share-title"?: string;
+		  }
+		| {
+				/** Text to copy */
+				text: string;
+		  }
+	);
 
 /**
  * Uses the
  * [Navigator API](https://developer.mozilla.org/en-US/docs/Web/API/Navigator/share)
- * to share a the value if it starts with `"http"` and `navigator.share` is supported.
+ * to share the `url` if `navigator.share` is supported.
+ *
  * Otherwise uses the
  * [Clipboard API](https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText)
- * to copy the value.
+ * to copy the `url` or `text` provided.
  *
  * ### Attributes
  *
- * `value`
+ * `url`
  *
- * Text to share.
+ * URL to share.
+ *
+ * `text`
+ *
+ * Text to copy, or the `ShareData` text if `url` is set (only supported on some targets).
+ *
+ * `share-title`
+ *
+ * `ShareData` title (only supported on some targets).
  */
 export class Share extends Lifecycle(Trigger(Content(Announce()))) {
 	constructor() {
 		super();
 	}
-	/**
-	 * The value to copy.
-	 *
-	 * @default ""
-	 */
-	get #value() {
-		return this.getAttribute("value") ?? "";
+
+	// helper since ShareData expects undefined instead of null
+	#attrOrUndefined(name: string) {
+		return this.getAttribute(name) ?? undefined;
 	}
 
-	/**
-	 * Shares or copies the `value`.
-	 */
-	#share(value = this.#value) {
-		if (
-			value.startsWith("http") &&
-			navigator.canShare &&
-			navigator.canShare({ url: value })
-		) {
-			return navigator.share({ url: value });
-		}
+	get #title() {
+		return this.#attrOrUndefined("share-title");
+	}
 
-		// fallback to copy
-		this.announce("copied text to clipboard");
-		this.swap();
-		return navigator.clipboard.writeText(value);
+	get #text() {
+		return this.#attrOrUndefined("text");
+	}
+
+	get #url() {
+		return this.#attrOrUndefined("url");
 	}
 
 	override mount() {
-		this.listener(() => this.#share());
+		this.listener(async () => {
+			const data: ShareData = {
+				title: this.#title,
+				text: this.#text,
+				url: this.#url,
+			};
+
+			if (data.url && navigator.canShare && navigator.canShare(data)) {
+				return navigator.share(data).catch((e) => {
+					// catch abort errors when user cancels the share
+					if (!(e instanceof Error) || e.name !== "AbortError") throw e;
+				});
+			}
+
+			const copy = data.url || data.text;
+
+			if (copy) {
+				return navigator.clipboard.writeText(copy).then(() => {
+					this.announce("copied to clipboard");
+					this.swap();
+				});
+			}
+		});
 	}
 }
